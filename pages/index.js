7f6b042a12d4adc58714ac812424896cb76d4393
Lawrence Hecht <LawrenceHecht@users.noreply.github.com>
2023-04-21 13:08:37 -0400

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -11,7 +11,7 @@ export default function Home() {
       </Head>
 
       <main>
-        <Header title="Welcome to my app!" />
+        <Header title=yo" />
         <p className="description">
           test <code>pages/index.js</code>
         </p>
